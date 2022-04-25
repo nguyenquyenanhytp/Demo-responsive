50c2c64bcf1f4c9ca949a18a120325e98a2b1690
unknown <nguyenquyenanhytp@gmail.com>
2022-04-25 15:11:05 +0700

    first commit

---
+++ assets/slick-slider/app.js
@@ -0,0 +1,36 @@
+// $(document).ready(function(){
+//     $('.feedback__wrap').slick({
+//         slidesToShow: 3,
+//         infinite: true,
+//         slidesToScroll: 1,
+//         autoplay : true,
+//         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+//         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+//         autoplaySpeed : 5000,
+//         responsive : [
+//             {
+//                 breakpoint: 1024,
+//                 settings: {
+//                   slidesToShow: 3,
+//                   slidesToScroll: 1,
+//                 }
+//               },
+//               {
+//                 breakpoint: 600,
+//                 settings: {
+//                   slidesToShow: 1,
+//                   slidesToScroll: 1
+//                 }
+//               },
+//               {
+//                 breakpoint: 480,
+//                 settings: {
+//                   slidesToShow: 1,
+//                   slidesToScroll: 1,
+//                 }
+//               }
+
+//         ]
+
+//     });
+//   });
